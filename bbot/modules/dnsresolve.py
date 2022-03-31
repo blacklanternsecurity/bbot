ff837b7bfcdfdb8156b620978fd0035afa8062bd
pmueller <pmueller@blacklanternsecurity.com>
2022-03-31 15:35:16 -0400

    forgot to run black ¯\_(ツ)_/¯

--- bbot/modules/dnsresolve.py
+++ bbot/modules/dnsresolve.py
@@ -1,12 +1,21 @@
 from .base import BaseModule
 import ipaddress
 
+
 class dnsresolve(BaseModule):
 
-    watched_events = ["IPV4_ADDRESS", "IPV4_RANGE", "IPV6_ADDRESS", "IPV6_RANGE", "DNS_NAME"]
+    watched_events = [
+        "IPV4_ADDRESS",
+        "IPV4_RANGE",
+        "IPV6_ADDRESS",
+        "IPV6_RANGE",
+        "DNS_NAME",
+    ]
     produced_events = ["IPV4_ADDRESS", "IPV6_ADDRESS", "DNS_NAME"]
-    options = {"max_hosts":65536}
-    options_desc = {"max_hosts":"Define the max number of hosts a network range can contain"}
+    options = {"max_hosts": 65536}
+    options_desc = {
+        "max_hosts": "Define the max number of hosts a network range can contain"
+    }
 
     def handle_event(self, event):
         # only process targets
@@ -16,29 +25,25 @@ class dnsresolve(BaseModule):
         if event.type == "DNS_NAME":
 
             self.debug(f"trying to resolve {event.data}")
-            self.resolve_arr([event.data],event)
+            self.resolve_arr([event.data], event)
 
         else:
             net = ipaddress.ip_network(event.data)
             if (event.type == "IPV4_RANGE") or (event.type == "IPV6_RANGE"):
-                config_max_hosts = self.config.get("max_hosts",65536)
+                config_max_hosts = self.config.get("max_hosts", 65536)
                 if net.num_addresses > config_max_hosts:
-                    self.debug(f"dns resolve exceeded max host count ({config_max_hosts} hosts). Got a (/{net.prefixlen}) network with ({net.num_addresses:,}) hosts.")
+                    self.debug(
+                        f"dns resolve exceeded max host count ({config_max_hosts} hosts). Got a (/{net.prefixlen}) network with ({net.num_addresses:,}) hosts."
+                    )
                     return
-            self.resolve_arr(net,event)
+            self.resolve_arr(net, event)
 
-    def resolve_arr(self,arr,event):
+    def resolve_arr(self, arr, event):
         futures = []
         for x in arr:
-            future = self.helpers.run_async(self.helpers.resolve,str(x))
+            future = self.helpers.run_async(self.helpers.resolve, str(x))
             futures.append(future)
         for future in self.helpers.as_completed(futures):
             r_set = future.result()
             if r_set:
-                self.emit_event(list(r_set)[0],source=event,tags=["resolved"])
-    
-
-
-
-
-                
+                self.emit_event(list(r_set)[0], source=event, tags=["resolved"])
