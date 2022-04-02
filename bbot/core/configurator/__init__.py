03b61a8cabbcd79550fa6e70d59de2605eee7999
TheTechromancer <thetechromancer@protonmail.com>
2022-04-02 13:21:46 -0400

    patch requests methods to disable ssl verification

--- bbot/core/configurator/__init__.py
+++ bbot/core/configurator/__init__.py
@@ -25,3 +25,12 @@ http_proxy = config.get("http_proxy", "")
 if http_proxy:
     os.environ["HTTP_PROXY"] = http_proxy
     os.environ["HTTPS_PROXY"] = http_proxy
+
+ssl_verify = config.get("ssl_verify", False)
+if not ssl_verify:
+    import requests
+    import functools
+    requests.adapters.BaseAdapter.send = functools.partialmethod(requests.adapters.BaseAdapter.send, verify=False)
+    requests.adapters.HTTPAdapter.send = functools.partialmethod(requests.adapters.HTTPAdapter.send, verify=False)
+    requests.Session.request = functools.partialmethod(requests.Session.request, verify=False)
+    requests.request = functools.partial(requests.request, verify=False)
