eb94ec0af969aa083fb6269ede8568ae9fbb5bf0
pmueller <pmueller@blacklanternsecurity.com>
2022-04-10 11:10:35 -0400

    added ffuf and telerik modules

---
+++ bbot/modules/telerik.py
@@ -0,0 +1,172 @@
+from .base import BaseModule
+from urllib.parse import urlparse
+
+
+class telerik(BaseModule):
+
+    watched_events = ["URL"]
+    produced_events = ["VULNERABILITY"]
+    in_scope_only = True
+    telerikVersions = [
+        "2007.1423",
+        "2007.1521",
+        "2007.1626",
+        "2007.2918",
+        "2007.21010",
+        "2007.21107",
+        "2007.31218",
+        "2007.31314",
+        "2007.31425",
+        "2008.1415",
+        "2008.1515",
+        "2008.1619",
+        "2008.2723",
+        "2008.2826",
+        "2008.21001",
+        "2008.31105",
+        "2008.31125",
+        "2008.31314",
+        "2009.1311",
+        "2009.1402",
+        "2009.1527",
+        "2009.2701",
+        "2009.2826",
+        "2009.31103",
+        "2009.31208",
+        "2009.31314",
+        "2010.1309",
+        "2010.1415",
+        "2010.1519",
+        "2010.2713",
+        "2010.2826",
+        "2010.2929",
+        "2010.31109",
+        "2010.31215",
+        "2010.31317",
+        "2011.1315",
+        "2011.1413",
+        "2011.1519",
+        "2011.2712",
+        "2011.2915",
+        "2011.31115",
+        "2011.3.1305",
+        "2012.1.215",
+        "2012.1.411",
+        "2012.2.607",
+        "2012.2.724",
+        "2012.2.912",
+        "2012.3.1016",
+        "2012.3.1205",
+        "2012.3.1308",
+        "2013.1.220",
+        "2013.1.403",
+        "2013.1.417",
+        "2013.2.611",
+        "2013.2.717",
+        "2013.3.1015",
+        "2013.3.1114",
+        "2013.3.1324",
+        "2014.1.225",
+        "2014.1.403",
+        "2014.2.618",
+        "2014.2.724",
+        "2014.3.1024",
+        "2015.1.204",
+        "2015.1.225",
+        "2015.2.604",
+        "2015.2.623",
+        "2015.2.729",
+        "2015.2.826",
+        "2015.3.930",
+        "2015.3.1111",
+        "2016.1.113",
+        "2016.1.225",
+        "2016.2.504",
+        "2016.2.607",
+        "2016.3.914",
+        "2016.3.1018",
+        "2016.3.1027",
+        "2017.1.118",
+        "2017.1.228",
+        "2017.2.503",
+        "2017.2.621",
+        "2017.2.711",
+        "2017.3.913",
+    ]
+    RAUConfirmed = []
+
+    options = {"skip_RAU_confirmation": False}
+    options_desc = {
+        "skip_RAU_confirmation": "Do not attempt to confirm any RAU AXD detections are vulnerable"
+    }
+
+    def handle_event(self, event):
+
+        result = self.test_detector(event.data, "Telerik.Web.UI.WebResource.axd?type=rau")
+        if result:
+            self.debug(result.text)
+            if "RadAsyncUpload handler is registered succesfully" in result.text:
+                self.debug(f"Detected Telerik instance (Telerik.Web.UI.WebResource.axd?type=rau)")
+                self.emit_event(
+                    "Telerik RAU AXD Handler detected", "VULNERABILITY", event, tags=["info"]
+                )
+
+                if self.config.get("skip_RAU_confirmation") == False:
+                    hostname = urlparse(event.data).netloc
+                    if hostname not in self.RAUConfirmed:
+                        self.RAUConfirmed.append(hostname)
+                        for version in self.telerikVersions:
+                            command = [
+                                "/usr/local/bin/python3",
+                                "/opt/telerik/RAU_crypto-master/RAU_crypto.py",
+                                "-P",
+                                "C:\\\\Windows\\\\Temp",
+                                version,
+                                "/opt/telerik/testfile.txt",
+                                result.url,
+                            ]
+                            output = self.helpers.run(command)
+                            if "fileInfo" in output.stdout:
+                                self.debug(f"Confirmed Vulnerable Telerik (version: {str(version)}")
+                                self.emit_event(
+                                    f"[CRITICAL] [CVE-2017-11317] [{str(version)}] Telerik.Web.UI.WebResource.axd?type=rau",
+                                    "VULNERABILITY",
+                                    event,
+                                    tags=["critical"],
+                                )
+                                break
+
+        result = self.test_detector(event.data, "Telerik.Web.UI.DialogHandler.aspx?dp=1")
+        if result:
+            if "Cannot deserialize dialog parameters" in result.text:
+                self.debug(f"Detected Telerik UI instance (Telerik.Web.UI.DialogHandler.aspx?dp=1)")
+                self.emit_event(
+                    "Telerik DialogHandler detected", "VULNERABILITY", event, tags=["info"]
+                )
+
+        result = self.test_detector(event.data, "Telerik.Web.UI.SpellCheckHandler.axd")
+        try:
+            if result.status_code == 500:
+                self.debug(f"Detected Telerik UI instance (Telerik.Web.UI.SpellCheckHandler.axd)")
+                self.emit_event(
+                    "Telerik SpellCheckHandler detected", "VULNERABILITY", event, tags=["info"]
+                )
+        except:
+            pass
+
+    def test_detector(self, baseurl, detector):
+
+        result = None
+        if "/" != baseurl[-1]:
+            url = f"{baseurl}/{detector}"
+        else:
+            url = f"{baseurl}{detector}"
+        result = self.helpers.request(url, timeout=5)
+        return result
+
+    def filter_event(self, event):
+
+        if ("endpoint" in event.tags) or ("uri" in event.tags):
+            return False
+        else:
+            return True
