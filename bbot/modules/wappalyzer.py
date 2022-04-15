5541a41e910391c4a3a829bdec1226e40b3f443a
pmueller <pmueller@blacklanternsecurity.com>
2022-04-15 14:08:13 -0400

    adding wappalyzer module

---
+++ bbot/modules/wappalyzer.py
@@ -0,0 +1,20 @@
+from .base import BaseModule
+from Wappalyzer import Wappalyzer, WebPage
+
+import warnings
+warnings.filterwarnings("ignore", message="""Caught 'unbalanced parenthesis at position 119' compiling regex""", category=UserWarning )
+
+
+class wappalyzer(BaseModule):
+
+    watched_events = ["URL"]
+    produced_events = ["TECHNOLOGY"]
+
+    def handle_event(self, event):
+
+        wappalyzer = Wappalyzer.latest()
+        r = self.helpers.request(event.data)
+        w = WebPage.new_from_response(r)
+        res_set = wappalyzer.analyze(w)
+        for res in res_set:
+            self.emit_event(res, "TECHNOLOGY", event, tags=["web"])
