485527c22bf62b6c3d915fd7114f1433c9e031b6
pmueller <pmueller@blacklanternsecurity.com>
2022-04-17 08:49:00 -0400

    More re-writing to wayback after testing. Maximum freshness guaranteed.

--- bbot/modules/wayback.py
+++ bbot/modules/wayback.py
@@ -9,14 +9,14 @@ class wayback(BaseModule):
     produced_events = ["URL"]
     scanned_hosts = []
 
+    dirs = set()
+    endpoints = set()
+    uris = set()
+
     options = {"include_params": False}
     options_desc = {"include_params": "Include URLs with query strings"}
 
     def handle_event(self, event):
-
-        dirs = set()
-        endpoints = set()
-        uris = set()
 
         parsed_host = urlparse(event.data)
         host = f"{parsed_host.scheme}://{parsed_host.netloc}/"
@@ -37,26 +37,42 @@ class wayback(BaseModule):
             if len(u) > 0:
                 u = u.replace(":80", "").replace(":443", "").rstrip()
                 if self.helpers.validate_url(u):
-                    test_request = self.helpers.request(u, cache_for=3600)
-                    if test_request:
-                        if test_request.status_code == 200:
-                            p = urlparse(u)
-                            p._replace(fragment="")._replace(query="")
+                    p = urlparse(u)
+                    p._replace(fragment="")._replace(query="")
+                    uri = p._replace(fragment="").geturl().rstrip()
+                    endpoint = p._replace(fragment="", query="").geturl().rstrip()
+                    dir = ("/".join(p._replace(fragment="", query="").geturl().split("/")[:-1]) + "/").rstrip()
 
-                            if self.config.get("include_params"):
-                                uris.add(p._replace(fragment="").geturl())
-                            endpoints.add(p._replace(fragment="", query="").geturl())
-                            found_dir = "/".join(p._replace(fragment="", query="").geturl().split("/")[:-1]) + "/"
-                            if found_dir != "https://" and found_dir != "http://":
-                                dirs.add(found_dir)
-                        else:
-                            self.debug(f"URL: {u} is not currently accessible, ignoring")
+                    if self.config.get("include_params"):
+                        if uri not in self.uris:
+                            self.uris.add(uri)
+                            test_request = self.helpers.request(uri, cache_for=3600)
+                            if test_request:
+                                if (test_request.status_code == 200) or (test_request.status_code == 500):
+                                    pass
+                                    # self.emit_event(uri, "URL", event, tags=["uri"])
+                            else:
+                                self.debug(f"URL: {uri} is not currently accessible, ignoring")
 
-        for dir in dirs:
-            self.emit_event(dir, "URL", event, tags=["dir"])
+                    if endpoint not in self.endpoints:
+                        self.endpoints.add(endpoint)
+                        test_request = self.helpers.request(endpoint, cache_for=3600)
+                        if test_request:
+                            if (test_request.status_code == 200) or (test_request.status_code == 500):
+                                self.emit_event(endpoint, "URL", event, tags=["endpoint"])
+                            else:
+                                self.debug(f"URL: {endpoint} is not currently accessible, ignoring")
 
-        for endpoint in endpoints:
-            self.emit_event(endpoint, "URL", event, tags=["endpoint"])
-        if self.config.get("include_params"):
-            for uri in uris:
-                self.emit_event(uri, "URL", event, tags=["uri"])
+                    if dir != "https://" and dir != "http://":
+                        if dir not in self.dirs:
+                            self.dirs.add(dir)
+                            test_request = self.helpers.request(u, cache_for=3600)
+                            if test_request:
+                                if (
+                                    (test_request.status_code == 200)
+                                    or (test_request.status_code == 500)
+                                    or (test_request.status_code == 403)
+                                ):
+                                    self.emit_event(dir, "URL", event, tags=["dir"])
+                            else:
+                                self.debug(f"URL: {dir} is not currently accessible, ignoring")
