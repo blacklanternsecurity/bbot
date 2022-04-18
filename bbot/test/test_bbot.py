b279130a5bb417740d12e4e57a7ead17857bbbb1
TheTechromancer <thetechromancer@protonmail.com>
2022-04-18 14:01:45 -0400

    updated tests, ipv6 support for dnsresolve module

--- bbot/test/test_bbot.py
+++ bbot/test/test_bbot.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import logging
 import ipaddress
 from time import sleep
@@ -141,6 +142,10 @@ def test_helpers():
     # resolution
     assert all([helpers.is_ip(i) for i in helpers.resolve("scanme.nmap.org")])
     assert "dns.google" in helpers.resolve("8.8.8.8")
+    assert "dns.google" in helpers.resolve("2001:4860:4860::8888")
+    resolved_ips = helpers.resolve("dns.google")
+    assert "2001:4860:4860::8888" in resolved_ips
+    assert "8.8.8.8" in resolved_ips
     assert any([helpers.is_subdomain(h) for h in helpers.resolve("google.com", type="mx")])
     v6_ips = helpers.resolve("www.google.com", type="AAAA")
     assert all([i.version == 6 for i in [ipaddress.ip_address(_) for _ in v6_ips]])
@@ -266,3 +271,16 @@ def test_agent():
     sleep(0.5)
     agent.scan_status()
     agent.stop_scan()
+
+
+def test_cli(monkeypatch):
+
+    from bbot import cli
+
+    monkeypatch.setattr(sys, "exit", lambda *args, **kwargs: True)
+    monkeypatch.setattr(sys, "argv", ["bbot", "-t", "api.publicapis.org", "-m", "dnsresolve"])
+    cli.main()
+    monkeypatch.setattr(sys, "argv", ["bbot", "--current-config"])
+    cli.main()
+    monkeypatch.setattr(sys, "argv", ["bbot", "-t", "api.publicapis.org", "-m", "plumbus"])
+    cli.main()
