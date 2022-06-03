182179c4ea76b98508f18238d09685ad4eb00cdb
TheTechromancer <thetechromancer@protonmail.com>
2022-06-03 14:57:20 -0400

    separated api/gui, updated to required python3.9, updated gitignore

--- api-drf/react/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme/build'
-import App from './App'
-
-it('mounts App without crashing', () => {
-  const wrapper = shallow(<App/>)
-  wrapper.unmount()
-})
