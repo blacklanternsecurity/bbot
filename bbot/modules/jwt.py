3beb2ae2f312141e620ebecdb25b703508224678
pmueller <paul.mueller@blacklanternsecurity.com>
2022-06-16 17:31:27 -0400

    added prototype jwt module

---
+++ bbot/modules/jwt.py
@@ -0,0 +1,32 @@
+from .base import BaseModule
+import json
+import re
+from jwt import PyJWT
+from jwt.exceptions import DecodeError
+
+
+class jwt(BaseModule):
+
+    watched_events = ["HTTP_RESPONSE"]
+    produced_events = ["INFO", "VULNERABILITY"]
+    deps_pip = ["PyJWT"]
+    suppress_dupes = False
+
+    def handle_event(self, event):
+
+        jwt_candidates = []
+        response_header = event.data["response-header"]
+        response_body = event.data["response-body"]
+        m = re.findall(r"eyJ(?:[\w-]*\.)(?:[\w-]*\.)[\w-]*", f"{response_header}{response_body}")
+
+        for result in m:
+            jwt_candidates.append(result)
+
+        for jwt_candidate in jwt_candidates:
+            j = PyJWT()
+
+            try:
+                j.decode(jwt_candidate, options={"verify_signature": False})
+                self.emit_event(f"JWT Identified [{jwt_candidate}]]", "INFO", event)
+            except DecodeError as e:
+                self.debug(f"Error decodeing JWT candidate {jwt_candidate}")
