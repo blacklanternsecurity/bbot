5d74d01cf078908024a74cadf437ff8e65335e96
TheTechromancer <thetechromancer@protonmail.com>
2022-06-23 16:09:40 -0400

    threadpool tests, removed 'all' module arg keyword

--- bbot/core/configurator/args.py
+++ bbot/core/configurator/args.py
@@ -28,12 +28,9 @@ class BBOTArgumentParser(argparse.ArgumentParser):
         ret.output_modules = chain_lists(ret.output_modules)
         ret.targets = chain_lists(ret.targets, try_files=True, msg="Reading targets from file: {filename}")
         ret.flags = chain_lists(ret.flags)
-        if "all" in ret.modules:
-            ret.modules = list(modules_preloaded)
-        else:
-            for m in ret.modules:
-                if m not in modules_preloaded and not self._dummy:
-                    raise ArgumentError(f'Module "{m}" is not valid. Choose from: {",".join(list(modules_preloaded))}')
+        for m in ret.modules:
+            if m not in modules_preloaded and not self._dummy:
+                raise ArgumentError(f'Module "{m}" is not valid. Choose from: {",".join(list(modules_preloaded))}')
         for m in ret.output_modules:
             if m not in output.modules_preloaded and not self._dummy:
                 raise ArgumentError(
@@ -61,14 +58,14 @@ for p in (parser, dummy_parser):
         "--modules",
         nargs="+",
         default=[],
-        help=f'Modules ("all" to enable all modules). Choices: {",".join(list(modules_preloaded))}',
+        help=f'Modules to enable. Choices: {",".join(list(modules_preloaded))}',
     )
     p.add_argument(
         "-f",
         "--flags",
         nargs="+",
         default=[],
-        help=f'Select modules by flag. Choices: {",".join(list(flag_choices))}',
+        help=f'Enable modules by flag. Choices: {",".join(list(flag_choices))}',
     )
     p.add_argument(
         "-o",
@@ -94,23 +91,22 @@ for p in (parser, dummy_parser):
     wordcloud = p.add_argument_group(
         title="Word cloud", description="Save/load wordlist of common words gathered during a scan"
     )
+    g1 = wordcloud.add_mutually_exclusive_group()
     wordcloud.add_argument("--save-wordcloud", help="Output wordcloud to file when the scan completes", metavar="FILE")
-    wordcloud.add_argument(
-        "--load-wordcloud", help="Load wordcloud from a file and use it in the scan", metavar="FILE"
-    )
-    wordcloud.add_argument(
+    g1.add_argument("--load-wordcloud", help="Load wordcloud from a file and use it in the scan", metavar="FILE")
+    g1.add_argument(
         "--load-last-wordcloud", action="store_true", help="Load the wordcloud from the last scan (from $BBOT_HOME)"
     )
     deps = p.add_argument_group(
         title="Module dependencies", description="Control how modules install their dependencies"
     )
-    g = deps.add_mutually_exclusive_group()
+    g2 = deps.add_mutually_exclusive_group()
     deps.add_argument(
         "--ignore-failed-deps", action="store_true", help="Run modules even if their dependency setup failed"
     )
-    g.add_argument("--no-deps", action="store_true", help="Don't install module dependencies")
-    g.add_argument("--force-deps", action="store_true", help="Force install all module dependencies")
-    g.add_argument("--retry-deps", action="store_true", help="Retry failed module dependencies")
+    g2.add_argument("--no-deps", action="store_true", help="Don't install module dependencies")
+    g2.add_argument("--force-deps", action="store_true", help="Force install all module dependencies")
+    g2.add_argument("--retry-deps", action="store_true", help="Retry failed module dependencies")
     agent = p.add_argument_group(title="Agent", description="Report back to a central server")
     agent.add_argument("-a", "--agent-mode", action="store_true", help="Start in agent mode")
 
