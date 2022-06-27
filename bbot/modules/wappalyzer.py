54a08ecb8638ee5b7c7cd9ec69c86e3a5785db07
pmueller <paul.mueller@blacklanternsecurity.com>
2022-06-27 15:53:44 -0400

    changed wappalyzer to use http_response

--- bbot/modules/wappalyzer.py
+++ bbot/modules/wappalyzer.py
@@ -13,16 +13,21 @@ warnings.filterwarnings(
 class wappalyzer(BaseModule):
 
     flags = ["active"]
-    watched_events = ["URL"]
+    watched_events = ["HTTP_RESPONSE"]
     produced_events = ["TECHNOLOGY"]
     deps_pip = ["python-Wappalyzer"]
-    in_scope_only = True
 
     def handle_event(self, event):
 
         wappalyzer = Wappalyzer.latest()
-        r = self.helpers.request(event.data)
-        w = WebPage.new_from_response(r)
+        split_headers = event.data["response-header"].split("\r\n")
+        header_dict = {}
+
+        for i in split_headers:
+            if len(i) > 0 and ":" in i:
+                header_dict[i.split(":")[0]] = i.split(":")[1].lstrip()
+
+        w = WebPage(event.data["url"], html=event.data["response-body"], headers=header_dict)
         res_set = wappalyzer.analyze(w)
         for res in res_set:
-            self.emit_event(f"[{event.data}] {res}", "TECHNOLOGY", event, tags=["web"])
+            self.emit_event(f"[{event.data['url']}] {res}", "TECHNOLOGY", event, tags=["web"])
