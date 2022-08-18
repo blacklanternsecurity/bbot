499d1bdf1203c88b4b178affcf3ce70f83e5a901
TheTechromancer <thetechromancer@protonmail.com>
2022-08-18 10:50:57 -0400

    change config location

--- bbot/core/configurator/files.py
+++ bbot/core/configurator/files.py
@@ -1,12 +1,17 @@
 from pathlib import Path
+from shutil import copyfile
 from omegaconf import OmegaConf
 
+from ..helpers.misc import mkdir
 from ..errors import ConfigLoadError
 
-home_dir = Path(__file__).parent.parent.parent.parent
-defaults_filename = (home_dir / "bbot" / "defaults.yml").resolve()
-config_filename = (home_dir / "bbot.yml").resolve()
-secrets_filename = (home_dir / "secrets.yml").resolve()
+config_dir = (Path.home() / ".config" / "bbot").resolve()
+defaults_filename = (Path(__file__).parent.parent.parent / "defaults.yml").resolve()
+defaults_destination = config_dir / "defaults.yml"
+mkdir(config_dir)
+copyfile(defaults_filename, defaults_destination)
+config_filename = (config_dir / "bbot.yml").resolve()
+secrets_filename = (config_dir / "secrets.yml").resolve()
 
 
 def _get_config(filename):
