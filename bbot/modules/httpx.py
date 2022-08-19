a1e54acb778c5557313aecf36c4d4a61c57ffd07
pmueller <paul.mueller@blacklanternsecurity.com>
2022-08-18 21:26:42 -0400

    filtering out _wildcards from becoming URL events

--- bbot/modules/httpx.py
+++ bbot/modules/httpx.py
@@ -38,6 +38,10 @@ class httpx(BaseModule):
         return True
 
     def filter_event(self, event):
+
+        if "_wildcard" in str(event.host).split("."):
+            return False
+
         if "unresolved" in event.tags:
             return False
 
@@ -45,6 +49,7 @@ class httpx(BaseModule):
             return False
 
         # scope filtering
+
         in_scope_only = self.config.get("in_scope_only", True)
         safe_to_visit = "httpx-safe" in event.tags
         if not safe_to_visit and (in_scope_only and not self.scan.in_scope(event)):
