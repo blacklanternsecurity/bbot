8af5cccd30c378992ae8d86aa2ee245e3253c5f8
TheTechromancer <thetechromancer@protonmail.com>
2022-08-24 14:41:47 -0400

    added rapiddns module

---
+++ bbot/modules/rapiddns.py
@@ -0,0 +1,24 @@
+from .crobat import crobat
+
+
+class rapiddns(crobat):
+
+    flags = ["subdomain-enum", "passive", "safe"]
+    watched_events = ["DNS_NAME"]
+    produced_events = ["DNS_NAME"]
+    meta = {"description": "Query rapiddns.io for subdomains"}
+
+    base_url = "https://rapiddns.io"
+
+    def request_url(self, query):
+        url = f"{self.base_url}/subdomain/{self.helpers.quote(query)}?full=1#result"
+        return self.helpers.request(url)
+
+    def parse_results(self, r, query):
+        results = set()
+        text = getattr(r, "text", "")
+        for match in self.helpers.regexes.dns_name_regex.findall(text):
+            match = match.lower()
+            if match.endswith(query):
+                results.add(match)
+        return results
