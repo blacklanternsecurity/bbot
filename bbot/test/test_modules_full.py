dee3acadec1537a5afd6c36758d4517a9c131b54
TheTechromancer <thetechromancer@protonmail.com>
2022-09-13 14:42:13 -0400

    force deps

--- bbot/test/test_modules_full.py
+++ bbot/test/test_modules_full.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 from pathlib import Path
+from omegaconf import OmegaConf
 
 from bbot import cli
 from .bbot_fixtures import bbot_config, ensure_root  # noqa: F401
@@ -10,8 +11,10 @@ log = logging.getLogger(f"bbot.test")
 
 def test_gowitness(monkeypatch, bbot_config, ensure_root):  # noqa: F811
 
+    config = OmegaConf.merge(bbot_config, OmegaConf.create({"force_deps": True}))
+
     monkeypatch.setattr(sys, "exit", lambda *args, **kwargs: True)
-    monkeypatch.setattr(cli, "config", bbot_config)
+    monkeypatch.setattr(cli, "config", config)
     monkeypatch.setattr(
         sys, "argv", ["bbot", "-y", "-m", "gowitness", "httpx", "-t", "http://www.example.com", "-n", "gowitness_test"]
     )
