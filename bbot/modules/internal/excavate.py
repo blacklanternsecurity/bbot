7e068546ccb6aec994256db275b2e74ec3abcef8
TheTechromancer <thetechromancer@protonmail.com>
2022-09-19 12:20:35 -0400

    excavate from both target and whitelist

--- bbot/modules/internal/excavate.py
+++ bbot/modules/internal/excavate.py
@@ -31,8 +31,10 @@ class HostnameExtractor(BaseExtractor):
     regexes = {}
 
     def __init__(self, excavate):
-        dns_targets = [t.host for t in excavate.scan.target if t.type == "DNS_NAME"]
-        dns_targets.sort(key=len)
+        dns_targets = set(t.host for t in excavate.scan.target if t.host and isinstance(t.host, str))
+        dns_whitelist = set(t.host for t in excavate.scan.whitelist if t.host and isinstance(t.host, str))
+        dns_targets.update(dns_whitelist)
+        dns_targets = sorted(dns_targets, key=len)
         dns_targets_set = set()
         for i, t in enumerate(dns_targets):
             if not any(x in dns_targets_set for x in excavate.helpers.domain_parents(t, include_self=True)):
