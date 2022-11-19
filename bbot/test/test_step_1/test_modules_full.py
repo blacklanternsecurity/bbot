dc1e8cd07adea5897d728ff9afbbb3ab467e7ade
liquidsec <paul.mueller08@gmail.com>
2022-11-19 02:13:07 -0500

    removing aspnet_viewstate test

--- bbot/test/test_step_1/test_modules_full.py
+++ bbot/test/test_step_1/test_modules_full.py
@@ -26,11 +26,6 @@ def test_httpx(bbot_config, bbot_scanner, bbot_httpserver):
     x.run()
 
 
-def test_aspnet_viewstate(bbot_config, bbot_scanner, bbot_httpserver):
-    x = Aspnet_viewstate(bbot_config, bbot_scanner, bbot_httpserver)
-    x.run()
-
-
 def test_getparam_brute(bbot_config, bbot_scanner, bbot_httpserver):
     x = Getparam_brute(bbot_config, bbot_scanner, bbot_httpserver)
     x.run()
