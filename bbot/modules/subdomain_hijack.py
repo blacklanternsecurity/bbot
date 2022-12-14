ccdb8aecb85e719844dce2038fb53f7911edf967
TheTechromancer <thetechromancer@protonmail.com>
2022-12-14 16:18:55 -0500

    match root domain

--- bbot/modules/subdomain_hijack.py
+++ bbot/modules/subdomain_hijack.py
@@ -3,6 +3,7 @@ import json
 import requests
 
 from bbot.modules.base import BaseModule
+from bbot.core.helpers.misc import tldextract
 
 
 class subdomain_hijack(BaseModule):
@@ -16,6 +17,7 @@ class subdomain_hijack(BaseModule):
     }
     options_desc = {"fingerprints": "URL or path to fingerprints.json"}
     scope_distance_modifier = 2
+    max_event_handlers = 5
 
     def setup(self):
         fingerprints_file = self.helpers.wordlist(self.config.get("fingerprints"))
@@ -28,7 +30,7 @@ class subdomain_hijack(BaseModule):
             except Exception as e:
                 self.warning(f"Error instantiating fingerprint: {e}")
                 continue
-            if not (f.cnames and f.vulnerable and f.fingerprint and f.autocheck_pass):
+            if not (f.domains and f.vulnerable and f.fingerprint and f.autocheck_pass):
                 self.debug(f"Skipping fingerprint: {f}")
                 continue
             self.debug(f"Processed fingerprint: {f}")
@@ -66,12 +68,13 @@ class subdomain_hijack(BaseModule):
 
     def check_subdomain(self, subdomain):
         for f in self.fingerprints:
-            for domain in f.cnames:
+            for domain in f.domains:
                 if self.helpers.host_in_host(subdomain, domain):
                     url = f"https://{subdomain}"
                     try:
-                        r = self.helpers.request(url)
-                        if not f.nxdomain and f.fingerprint_regex.findall(r.text):
+                        r = self.helpers.request(url, raise_error=True)
+                        text = getattr(r, "text", "")
+                        if not f.nxdomain and f.fingerprint_regex.findall(text):
                             return True, subdomain
                     except requests.exceptions.RequestException as e:
                         if f.nxdomain and "Name or service not known" in str(e):
@@ -84,8 +87,9 @@ class Fingerprint:
         assert isinstance(fingerprint, dict), "fingerprint must be a dictionary"
         self.engine = fingerprint.get("service")
         self.cnames = fingerprint.get("cname", [])
+        self.domains = list(set([tldextract(c).registered_domain for c in self.cnames]))
         self.nxdomain = fingerprint.get("nxdomain", False)
-        self.vulnerable = fingerprint.get("status", "Not vulnerable").capitalize() == "Vulnerable"
+        self.vulnerable = fingerprint.get("vulnerable", False)
         self.fingerprint = fingerprint.get("fingerprint", "")
         self.autocheck_pass = fingerprint.get("autocheck_pass", False)
         try:
