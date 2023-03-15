5fda2cca04d2e103078ec4fa8c07cdb8bf73528e
liquidsec <paul.mueller08@gmail.com>
2023-03-15 15:30:58 -0400

    adding additional nuclei messages

--- bbot/modules/deadly/nuclei.py
+++ bbot/modules/deadly/nuclei.py
@@ -70,8 +70,14 @@ class nuclei(BaseModule):
         self.budget = int(self.config.get("budget", 1))
         self.templates = self.config.get("templates")
         self.tags = self.config.get("tags")
+        if self.tags:
+            self.info(f"Setting the following nuclei tags: [{','.join([self.tags])}]")
         self.etags = self.config.get("etags")
+        if self.etags:
+            self.info(f"Excluding the following nuclei tags: [{','.join([self.etags])}]")
         self.severity = self.config.get("severity")
+        if self.mode != "severe" and self.severity != "":
+            self.info(f"Limiting nuclei templates to the following severites: [{','.join([self.severity])}]")
         self.iserver = self.scan.config.get("interactsh_server", None)
         self.itoken = self.scan.config.get("interactsh_token", None)
 
