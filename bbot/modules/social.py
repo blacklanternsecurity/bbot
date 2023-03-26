b17ea8a6887b3f7349a7369b18d8a0b30594a63b
TheTechromancer <thetechromancer@protonmail.com>
2023-03-26 15:37:29 -0400

    update regexes

--- bbot/modules/social.py
+++ bbot/modules/social.py
@@ -9,15 +9,15 @@ class social(BaseModule):
     flags = ["active", "social-enum"]
 
     social_media_regex = {
-        "linkedin": r"(https?:\/\/)?(www\.)?linkedin\.com\/(in|company)\/[a-zA-Z0-9-]+\/?",
-        "facebook": r"(https?:\/\/)?(www\.)?facebook\.com\/[a-zA-Z0-9\.]+\/?",
-        "twitter": r"(https?:\/\/)?(www\.)?twitter\.com\/[a-zA-Z0-9_]{1,15}\/?",
-        "github": r"(https?:\/\/)?(www\.)?github\.com\/[a-zA-Z0-9_-]+\/?",
-        "instagram": r"(https?:\/\/)?(www\.)?instagram\.com\/[a-zA-Z0-9_\.]+\/?",
-        "youtube": r"(https?:\/\/)?(www\.)?youtube\.com\/[a-zA-Z0-9_]+\/?",
-        "bitbucket": r"(https?:\/\/)?(www\.)?bitbucket\.org\/[a-zA-Z0-9_-]+\/?",
-        "gitlab": r"(https?:\/\/)?(www\.)?gitlab\.com\/[a-zA-Z0-9_-]+\/?",
-        "discord": r"(https?:\/\/)?(www\.)?discord\.gg\/[a-zA-Z0-9_-]+\/?",
+        "linkedin": r"(?:https?:\/\/)?(?:www\.)?linkedin\.com\/(?:in|company)\/[a-zA-Z0-9-]+\/?",
+        "facebook": r"(?:https?:\/\/)?(?:www\.)?facebook\.com\/[a-zA-Z0-9\.]+\/?",
+        "twitter": r"(?:https?:\/\/)?(?:www\.)?twitter\.com\/[a-zA-Z0-9_]{1,15}\/?",
+        "github": r"(?:https?:\/\/)?(?:www\.)?github\.com\/[a-zA-Z0-9_-]+\/?",
+        "instagram": r"(?:https?:\/\/)?(?:www\.)?instagram\.com\/[a-zA-Z0-9_\.]+\/?",
+        "youtube": r"(?:https?:\/\/)?(?:www\.)?youtube\.com\/[a-zA-Z0-9_]+\/?",
+        "bitbucket": r"(?:https?:\/\/)?(?:www\.)?bitbucket\.org\/[a-zA-Z0-9_-]+\/?",
+        "gitlab": r"(?:https?:\/\/)?(?:www\.)?gitlab\.com\/[a-zA-Z0-9_-]+\/?",
+        "discord": r"(?:https?:\/\/)?(?:www\.)?discord\.gg\/[a-zA-Z0-9_-]+\/?",
     }
 
     scope_distance_modifier = 0
@@ -28,6 +28,6 @@ class social(BaseModule):
 
     def handle_event(self, event):
         for platform, regex in self.compiled_regexes.items():
-            if regex.match(event.data):
-                social_media_links = {"platform": platform, "url": event.data}
+            for match in regex.findall(event.data):
+                social_media_links = {"platform": platform, "url": match}
                 self.emit_event(social_media_links, "SOCIAL", source=event)
