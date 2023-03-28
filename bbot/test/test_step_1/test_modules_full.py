3d390e89f034e0aa46b99296814be6746c1bb9f7
liquidsec <paul.mueller08@gmail.com>
2023-03-28 18:28:11 -0400

    black :( :(

--- bbot/test/test_step_1/test_modules_full.py
+++ bbot/test/test_step_1/test_modules_full.py
@@ -156,9 +156,11 @@ def test_hunt(bbot_config, bbot_scanner, bbot_httpserver):
     x = Hunt(bbot_config, bbot_scanner, bbot_httpserver)
     x.run()
 
+
 def test_speculate_subdirectories(bbot_config, bbot_scanner, bbot_httpserver):
     x = Speculate_subdirectories(bbot_config, bbot_scanner, bbot_httpserver, module_name="speculate")
     x.run()
+
 
 def test_social(bbot_config, bbot_scanner, bbot_httpserver):
     x = Social(bbot_config, bbot_scanner, bbot_httpserver)
