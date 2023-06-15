5f23ecfc91b289fe12e58d464f0b1a39df2bbdf6
TheTechromancer <thetechromancer@protonmail.com>
2023-06-15 11:47:24 -0400

    blacked

--- bbot/test/test_step_2/module_tests/test_module_sslcert.py
+++ bbot/test/test_step_2/module_tests/test_module_sslcert.py
@@ -5,4 +5,6 @@ class TestSSLCert(ModuleTestBase):
     targets = ["127.0.0.1:9999", "bbottest.notreal"]
 
     def check(self, module_test, events):
-        assert any(e.data == "www.bbottest.notreal" and str(e.module) == "sslcert" for e in events), "Failed to detect subdomain"
+        assert any(
+            e.data == "www.bbottest.notreal" and str(e.module) == "sslcert" for e in events
+        ), "Failed to detect subdomain"
