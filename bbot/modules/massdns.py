312e7c9e6bc6563416d631960f84c21175221869
TheTechromancer <thetechromancer@protonmail.com>
2023-07-24 15:01:02 -0400

    speed up massdns

--- bbot/modules/massdns.py
+++ bbot/modules/massdns.py
@@ -130,7 +130,7 @@ class massdns(crobat):
                 return []
         self.verbose(f"Resolving batch of {len(results):,} results")
         resolved = dict(
-            [l async for l in self.helpers.resolve_batch(results, type=("A", "AAAA", "CNAME"), cache_result=True)]
+            [l async for l in self.helpers.resolve_batch(results, type=("A", "CNAME"), cache_result=True)]
         )
         resolved = {k: v for k, v in resolved.items() if v}
         for hostname in resolved:
