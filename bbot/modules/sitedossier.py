3ed610e9032b036c4dddf727581121c3a220b75b
TheTechromancer <thetechromancer@protonmail.com>
2023-07-30 23:03:14 -0400

    added sitedossier module

---
+++ bbot/modules/sitedossier.py
@@ -0,0 +1,34 @@
+from .crobat import crobat
+
+
+class sitedossier(crobat):
+    flags = ["subdomain-enum", "passive", "safe"]
+    watched_events = ["DNS_NAME"]
+    produced_events = ["DNS_NAME"]
+    meta = {"description": "Query sitedossier.com for subdomains"}
+
+    base_url = "http://www.sitedossier.com/parentdomain"
+    max_pages = 10
+
+    async def query(self, query, parse_fn=None, request_fn=None):
+        results = set()
+        base_url = f"{self.base_url}/{self.helpers.quote(query)}"
+        url = str(base_url)
+        for page in range(1, 100 * self.max_pages + 2, 100):
+            if page > 1:
+                url = f"{base_url}/{page}"
+            response = await self.request_with_fail_count(url)
+            if response is None:
+                self.info(f'Query "{query}" failed (no response)')
+                return results
+            if response.status_code == 302:
+                self.verbose("Hit rate limit captcha")
+                break
+            for regex in self.scan.dns_regexes:
+                for match in regex.finditer(response.text):
+                    hostname = match.group().lower()
+                    results.add(hostname)
+            if "Show next 100 items" not in response.text:
+                break
+
+        return results
