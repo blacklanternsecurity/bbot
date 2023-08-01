ad1e04cb0f7d74b470f6503e0cdfddec53e986b8
TheTechromancer <thetechromancer@protonmail.com>
2023-08-01 10:13:47 -0400

    hard-pinned anyio version, better logging

--- bbot/scanner/manager.py
+++ bbot/scanner/manager.py
@@ -460,13 +460,11 @@ class ScanManager:
 
             if modules_status:
                 modules_status_str = ", ".join([f"{m}({i:,}:{t:,}:{o:,})" for m, r, i, o, t, _ in modules_status])
-                running_modules_str = ", ".join([m[0] for m in modules_status if m[1]])
-                if not running_modules_str:
-                    running_modules_str = "None"
-                self.scan.info(f"{self.scan.name}: Modules running: {running_modules_str}")
-                self.scan.verbose(
-                    f"{self.scan.name}: Modules status (incoming:processing:outgoing) {modules_status_str}"
+                self.scan.info(
+                    f"{self.scan.name}: Modules running (incoming:processing:outgoing) {modules_status_str}"
                 )
+            else:
+                self.scan.info(f"{self.scan.name}: No modules running")
             event_type_summary = sorted(
                 self.scan.stats.events_emitted_by_type.items(), key=lambda x: x[-1], reverse=True
             )
