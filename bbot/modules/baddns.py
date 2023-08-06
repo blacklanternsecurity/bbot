ad2809160909220c1613d05eebacb10c2f50d3c0
liquidsec <paul.mueller08@gmail.com>
2023-08-05 22:20:42 -0400

    baddns prototype

---
+++ bbot/modules/baddns.py
@@ -0,0 +1,23 @@
+from baddns.lib import baddns as BadDNS
+
+from .base import BaseModule
+
+class baddns(BaseModule):
+    watched_events = ["DNS_NAME","DNS_NAME_UNRESOLVED"]
+    produced_events = ["FINDING", "VULNERABILITY"]
+    flags = ["active", "safe", "web-basic"]
+    meta = {"description": "Check subdomains for for subdomain takeovers and other DNS tomfoolery"}
+    max_event_handlers = 2
+    deps_pip = ["baddns"]
+
+    async def handle_event(self, event):
+        baddns_cname = BadDNS.BadDNS_cname(event.data)
+        if await baddns_cname.dispatch():
+            r = baddns_cname.analyze()
+            if r:
+                data = {
+                    "severity": "MEDIUM",
+                    "description": f"Probable Subdomain Takeover. CNAME: [{r['cname']}] Signature Name: [{r['signature_name']}] Matching Domain: [{r['matching_domain']}] Technique: [{r['Technique']}]",
+                    "host": str(event.host),
+                }
+                self.emit_event(data, "VULNERABILITY", event)
