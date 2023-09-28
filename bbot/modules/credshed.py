1ee1caffa35c73221bbbc9d65759f3da5ad41a9a
SpamFaux <samu3lf0x@gmail.com>
2023-09-28 18:05:05 -0500

    Created a credshed and dehashed module for bbot.

---
+++ bbot/modules/credshed.py
@@ -0,0 +1,103 @@
+from bbot.modules.base import BaseModule
+
+class credshed(BaseModule):
+    watched_events = ["EMAIL_ADDRESS","DNS_NAME"]
+    produced_events = ["PASSWORD","HASHED_PASSWORD","USERNAME"]
+    flags = ["passive"]
+    meta = {"description":"Send queries to your own credshed server to check for known credentials of your targets", "auth_required": True}
+    options = {"username":"", "password":"", "credshed_url":""}
+
+
+    async def setup(self):
+        self.base_url = self.config.get("credshed_url","")
+        self.username = self.config.get("username","")
+        self.password = self.config.get("password","")
+        self.results = {}
+
+        auth_setup = await self.helpers.request(f"{self.base_url}/api/auth",
+                                      method="POST",
+                                      json={
+                                          "username":self.username,
+                                          "password":self.password
+                                      }
+                                      )
+        self.auth_token = auth_setup.json().get('access_token')
+
+        return await super().setup()
+    
+    async def filter_event(self, event):
+        if event.module == self or "subdomain" in event.tags:
+            return False
+        return True
+        
+    async def handle_event(self, event):
+        cs_query = await self.helpers.request(f"{self.base_url}/api/search", method="POST", cookies={'access_token_cookie':self.auth_token}, json={'query':event.data})
+        
+        if cs_query and cs_query.json().get('stats').get("total_count") > 0:
+            accounts = cs_query.json().get('accounts')
+            for i in accounts:
+                email = i.get("e")
+                pw = i.get("p")
+                h_pw = i.get("h")
+                user = i.get("u")
+                src = i.get("s")[0]
+                if email not in self.results:
+                    self.results[email] = {
+                        "source":[src],
+                        "passwords":{},
+                        "hashed":{},
+                        "usernames":{}
+                    }
+                else:
+                    if src not in self.results[email]["source"]:
+                        self.results[email]["source"].append(src)
+
+                if pw:
+                    if pw not in self.results[email]["passwords"]:
+                        self.results[email]["passwords"][pw] = [src]
+                    else:
+                        self.results[email]["passwords"][pw].append(src)
+                
+                if h_pw:
+                    for x in h_pw:
+                        if x not in self.results[email]["hashed"]:
+                            self.results[email]["hashed"][x] = [src]
+                        else:
+                            self.results[email]["hashed"][x].append(src)
+                
+                if user:
+                    if user not in self.results[email]["usernames"]:
+                        self.results[email]["usernames"][user] = [src]
+                    else:
+                        self.results[email]["usernames"][user].append(src)
+
+
+            for x in self.results:
+                if cs_query.json().get('stats').get('query_type') == "domain":
+                   f = self.make_event(x, "EMAIL_ADDRESS", source=event, tags='credshed')
+                   self.emit_event(f)
+
+                   if self.results[x]["hashed"]:
+                       for y in self.results[x]["hashed"]:
+                           self.emit_event(y, "HASHED_PASSWORD",source=f, tags=f'credshed-source-{self.results[x]["hashed"][y]}')
+
+                   if self.results[x]["passwords"]:
+                       for y in self.results[x]["passwords"]:
+                           self.emit_event(y, "PASSWORD", source=f, tags=f'credshed-source-{self.results[x]["passwords"][y]}')
+                    
+                   if self.results[x]["usernames"]:
+                       for y in self.results[x]["usernames"]:
+                           self.emit_event(y, "USERNAME", source=f, tags=f'credshed-source-{self.results[x]["usernames"][y]}')
+
+                if cs_query.json().get('stats').get('query_type') == "email":
+                    if self.results[x]["hashed"]:
+                       for y in self.results[x]["hashed"]:
+                           self.emit_event(y, "HASHED_PASSWORD",source=event, tags=f'credshed-source-{self.results[x]["hashed"][y]}')
+
+                    if self.results[x]["passwords"]:
+                       for y in self.results[x]["passwords"]:
+                           self.emit_event(y, "PASSWORD", source=event, tags=f'credshed-source-{self.results[x]["passwords"][y]}')
+                    
+                    if self.results[x]["usernames"]:
+                       for y in self.results[x]["usernames"]:
+                           self.emit_event(y, "USERNAME", source=event, tags=f'credshed-source-{self.results[x]["usernames"][y]}')
--- bbot/modules/dehashed.py
+++ bbot/modules/dehashed.py
@@ -2,72 +2,159 @@ from bbot.modules.base import BaseModule
 from requests.auth import HTTPBasicAuth
 
 class dehashed(BaseModule):
-    watched_events = ["EMAIL_ADDRESS"]
-    produced_events = ["PASSWORD"]
+    watched_events = ["EMAIL_ADDRESS","DNS_NAME"]
+    produced_events = ["PASSWORD","HASHED_PASSWORD","USERNAME"]
     flags = ["passive"]
-    meta = {"description": "Execute queries against dehash.com for exposed credentials", "auth_required": True}
-    options = {"username":"", "api_key": ""}
-    options_desc = {"username":"Email Address associated with your API key","api_key": "API Key"}
+    meta = {"description": "Execute queries against dehashed.com for exposed credentials", "auth_required": True}
+    options = {"username":"", "api_key":"", "check_domains":False}
+    options_desc = {"username":"Email Address associated with your API key","api_key": "API Key", "check_domains":"Enable only if bbot should search dehashed for new email addresses - this can be significantly more expensive against the API."}
 
     base_url = "https://api.dehashed.com/search"
 
-    def setup(self):
+    async def setup(self):
         self.username = self.config.get("username", "")
         self.api_key = self.config.get("api_key","")
         self.auth = HTTPBasicAuth(self.username, self.api_key)
         self.headers = {
             "Accept": "application/json",
         }
+        self.check_domains = self.config.get("check_domains",False)
 
         self.result_passwords = {}
         self.result_usernames = {}
         self.result_hashed_passwords = {}
+        self.result_email = {}
+        self.api_balance = 0
+        self.api_count = 0
+        self.api_warn = False
 
-        return super().setup()
+        return await super().setup()
 
-    def handle_event(self, event):
-    
+    async def filter_event(self, event):
+        if event.module == self or "subdomain" in event.tags:
+            return False
+        return True
+
+    def parse_results(self, result, isDNS):
+        # If there are no results then leave the function
+        if result.json().get('total') == 0:
+            return
+        
+        self.api_balance = result.json().get('balance')
+        self.api_count = result.json().get('total')
+
+        entries = result.json().get("entries")
+        
+
+        # parses the entries
+        for i in entries:
+            user = i.get("username")
+            pw = i.get("password")
+            h_pw = i.get("hashed_password")
+            email = i.get("email")
+            db_name = i.get("database_name")
+
+            if isDNS:
+                if email not in self.result_email:
+                    self.result_email[email] = {
+                        "source":[db_name],
+                        "passwords":{},
+                        "hashed":{},
+                        "usernames":{}
+                        }
+                else:
+                    self.result_email[email]["source"].append(db_name)
+
+                if pw:
+                    if pw not in self.result_email[email]["passwords"]:
+                        self.result_email[email]["passwords"][pw] = [db_name]
+                    else:
+                        self.result_email[email]["passwords"][pw].append(db_name)
+                
+                if h_pw:
+                    if h_pw not in self.result_email[email]["hashed"]:
+                        self.result_email[email]["hashed"][h_pw] = [db_name]
+                    else:
+                        self.result_email[email]["hashed"][h_pw].append(db_name)
+
+            else:   
+                if pw:
+                    if pw not in self.result_passwords:
+                        self.result_passwords[pw] = [db_name]
+                    else:
+                        self.result_passwords[pw].append(db_name)
+                
+                # If there is a hashed password result then adds it to dict
+                if h_pw:
+                    if h_pw not in self.result_passwords:
+                        self.result_hashed_passwords[h_pw] = [db_name]
+                    else:
+                        self.result_hashed_passwords[h_pw].append(db_name)
+                
+                # If there is a username result then adds it to dict
+                if user:
+                    if user not in self.result_usernames:
+                        self.result_usernames[user] = [db_name]
+                    else:
+                        self.result_usernames[user].append(db_name)
+
+           
+
+    async def handle_event(self, event):
+        # Checks if user explicitly opted in for domain checking
+        if self.check_domains:
+            if event.type == "DNS_NAME":
+                url = f"{self.base_url}?query=domain:{event.data}&size=10000&page=1"
+                result = await self.helpers.request(url, auth=self.auth, headers=self.headers)
+                
+                if result:
+                    self.parse_results(result,True)
+
+                    # Check to see if multiple requests are required
+                    if self.api_count > 10000:
+                        pages = self.api_count // 10000
+                        if self.api_count % 10000:
+                            pages += 1
+
+                        for i in range(2, pages+1):
+                            if i <= 3:
+                                url = f"{self.base_url}?query=domain:{event.data}&size=10000&page={i}"
+                                result = await self.helpers.request(url, auth=self.auth, headers=self.headers)
+                                self.parse_results(result,True)
+                            # Due to a limitation in the api we cannot request more thank 30k results or 3 pages of 10k - if we go beyond the 3 pages then break out and warn
+                            elif i > 3:
+                                self.api_warn = True
+                                break
+
+                    for x in self.result_email:
+                        f = self.make_event(x, "EMAIL_ADDRESS", source=event, tags=self.result_email[x]["source"])
+                        self.emit_event(f)
+                        if self.result_email[x]["hashed"]:
+                            for y in self.result_email[x]["hashed"]:
+                                self.emit_event(y, "HASHED_PASSWORD", source=f, tags=self.result_email[x]["hashed"][y])
+                        if self.result_email[x]["passwords"]:
+                            for y in self.result_email[x]["passwords"]:
+                                self.emit_event(y, "PASSWORD", source=f, tags=self.result_email[x]["passwords"][y])
+                        if self.result_email[x]["usernames"]:
+                            for y in self.result_email[x]["usernames"]:
+                                self.emit_event(y, "USERNAME", source=f, tags=self.result_email[x]["usernames"][y])
+
+
         if event.type == "EMAIL_ADDRESS":
             url = f"{self.base_url}?query=email:{event.data}"
-            result = self.helpers.request(url, auth=self.auth, headers=self.headers)
+            result = await self.helpers.request(url, auth=self.auth, headers=self.headers)
             if result:
-                result_json = result.json()
-                entries = result_json.get("entries")
-                for i in entries:
-                    user = i.get("username")
-                    pw = i.get("password")
-                    h_pw = i.get("hashed_password")
-                    db_name = i.get("database_name")
-                    
-                    # If there is a password result then adds it to dict
-                    if pw:
-                        self.emit_event(pw, "PASSWORD", source=event)
-                        if pw not in self.result_passwords:
-                            self.result_passwords[pw] = [db_name]
-                        else:
-                            self.result_passwords[pw].append(db_name)
-                    
-                    # If there is a hashed password result then adds it to dict
-                    if h_pw:
-                        if h_pw not in self.result_passwords:
-                            self.result_hashed_passwords[h_pw] = [db_name]
-                        else:
-                            self.result_hashed_passwords[h_pw].append(db_name)
-                    
-                    # If there is a username result then adds it to dict
-                    if user:
-                        if user not in self.result_usernames:
-                            self.result_usernames[user] = [db_name]
-                        else:
-                            self.result_usernames[user].append(db_name)
-"""
+                self.parse_results(result,False)
+
                 if self.result_passwords:
                     for x in self.result_passwords:
-                        self.emit_event(x, "PASSWORD", source=event)
+                        self.emit_event(x, "PASSWORD", source=event, tags=self.result_passwords[x])
                 if self.result_hashed_passwords:
-                    #self.hugesuccess(self.result_hashed_passwords)
                     for x in self.result_hashed_passwords:
-                        self.emit_event(x, "HASHED_PASSWORD", source=event)
+                        self.emit_event(x, "HASHED_PASSWORD", source=event, tags=self.result_hashed_passwords[x])
                 if self.result_usernames:
-                    self.hugesuccess(self.result_usernames)
-"""
+                    for x in self.result_usernames:
+                        self.emit_event(x, "USERNAME", source=event, tags=self.result_usernames[x])
+        
+        if self.api_warn:
+            self.info(f"{event.data} has {self.api_count} results in Dehashed. The API can only process the first 30,000 results. Please check dehashed.com to get the remaining results.")
