f0e242b3808fac047e7d213f652b9bcfbb8d3dec
TheTechromancer <thetechromancer@protonmail.com>
2023-12-18 15:02:25 -0500

    account for asset inventory use_previous

--- bbot/modules/templates/portscanner.py
+++ bbot/modules/templates/portscanner.py
@@ -19,11 +19,23 @@ class portscanner(BaseModule):
         return True
 
     async def filter_event(self, event):
-        # skip IP_ADDRESSes if they are included in any of our target IP_RANGEs
-        if event.type == "IP_ADDRESS":
+        """
+        The purpose of this filter_event is to decide whether we should accept individual IP_ADDRESS
+        events that reside inside our target subnets (IP_RANGE), if any.
+
+        This prevents scanning the same IP twice.
+        """
+        # if we are emitting hosts from a previous asset_inventory, this is a special case
+        # in this case we want to accept the individual IPs even if they overlap with our target ranges
+        asset_inventory_module = self.scan.modules.get("asset_inventory", None)
+        asset_inventory_config = getattr(asset_inventory_module, "config", {})
+        asset_inventory_use_previous = asset_inventory_config.get("use_previous", False)
+        if event.type == "IP_ADDRESS" and not asset_inventory_use_previous:
             for net in self.helpers.ip_network_parents(event.data, include_self=True):
                 if net in self.ip_ranges:
-                    return False, f"Skipping {event.host} because it is already included in {net}"
+                    return False, f"skipping {event.host} because it is already included in {net}"
+        elif event.type == "IP_RANGE" and asset_inventory_use_previous:
+            return False, f"skipping IP_RANGE {event.host} because asset_inventory.use_previous=True"
         return True
 
     def _build_targets(self, target, delimiter=","):
