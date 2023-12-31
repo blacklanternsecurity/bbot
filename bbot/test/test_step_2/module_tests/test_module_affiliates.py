42fe420c23b3ba39a527e843af7aea7132905838
TheTechromancer <thetechromancer@protonmail.com>
2023-12-30 20:30:59 -0500

    blacked

--- bbot/test/test_step_2/module_tests/test_module_affiliates.py
+++ bbot/test/test_step_2/module_tests/test_module_affiliates.py
@@ -6,12 +6,14 @@ class TestAffiliates(ModuleTestBase):
     config_overrides = {"dns_resolution": True}
 
     async def setup_before_prep(self, module_test):
-        module_test.scan.helpers.dns.mock_dns({
-            ("8.8.8.8", "PTR"): "dns.google",
-            ("dns.google", "A"): "8.8.8.8",
-            ("dns.google", "NS"): "ns1.zdns.google",
-            ("ns1.zdns.google", "A"): "1.2.3.4",
-        })
+        module_test.scan.helpers.dns.mock_dns(
+            {
+                ("8.8.8.8", "PTR"): "dns.google",
+                ("dns.google", "A"): "8.8.8.8",
+                ("dns.google", "NS"): "ns1.zdns.google",
+                ("ns1.zdns.google", "A"): "1.2.3.4",
+            }
+        )
 
     def check(self, module_test, events):
         filename = next(module_test.scan.home.glob("affiliates-table*.txt"))
