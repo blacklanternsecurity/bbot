6ede3229f9f0d7a1d945d717ccda71ce98b07f92
TheTechromancer <thetechromancer@protonmail.com>
2024-02-01 17:43:02 -0500

    JSON module: option for SIEM-friendly output

--- bbot/modules/output/json.py
+++ bbot/modules/output/json.py
@@ -7,16 +7,24 @@ from bbot.modules.output.base import BaseOutputModule
 class JSON(BaseOutputModule):
     watched_events = ["*"]
     meta = {"description": "Output to Newline-Delimited JSON (NDJSON)"}
-    options = {"output_file": "", "console": False}
-    options_desc = {"output_file": "Output to file", "console": "Output to console"}
+    options = {"output_file": "", "console": False, "siem_friendly": False}
+    options_desc = {
+        "output_file": "Output to file",
+        "console": "Output to console",
+        "siem_friendly": "Output JSON in a SIEM-friendly format for ingestion into Elastic, Splunk, etc.",
+    }
     _preserve_graph = True
 
     async def setup(self):
         self._prep_output_dir("output.ndjson")
+        self.siem_friendly = self.config.get("siem_friendly", False)
         return True
 
     async def handle_event(self, event):
-        event_str = json.dumps(dict(event))
+        event_json = dict(event)
+        if self.siem_friendly:
+            event_json["data"] = {event.type: event_json.pop("data", "")}
+        event_str = json.dumps(event_json)
         if self.file is not None:
             self.file.write(event_str + "\n")
             self.file.flush()
