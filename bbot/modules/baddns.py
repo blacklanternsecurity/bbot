b3bfd1a711ff0e778b533d7e271c3fed61ca5cc7
liquidsec <paul.mueller08@gmail.com>
2024-02-05 14:39:27 -0500

    rolling version numbers again for python 3.12 fix

--- bbot/modules/baddns.py
+++ bbot/modules/baddns.py
@@ -18,7 +18,7 @@ class baddns(BaseModule):
         "only_high_confidence": "Do not emit low-confidence or generic detections",
     }
     max_event_handlers = 8
-    deps_pip = ["baddns~=1.0.698"]
+    deps_pip = ["baddns~=1.0.702"]
 
     def select_modules(self):
         selected_modules = []
