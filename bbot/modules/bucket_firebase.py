cfde2db2d1cb0869fd39c123e03a25f4c06e4f69
osmith <oliver.smith@cybercx.com.au>
2024-02-21 09:42:50 +1100

    linting

--- bbot/modules/bucket_firebase.py
+++ bbot/modules/bucket_firebase.py
@@ -20,7 +20,7 @@ class bucket_firebase(bucket_template):
         if not any(host.endswith(f".{d}") for d in self.base_domains):
             return False, "bucket belongs to a different cloud provider"
         return True, ""
-        
+
     async def check_bucket_exists(self, bucket_name, url):
         url = url.strip("/") + "/.json"
         if "_" in bucket_name:
