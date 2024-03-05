21aa57078d61c3240601079f27e5d7d1c6e9bd92
Jack Ward <stryker2k2@msn.com>
2024-03-05 15:06:38 -0600

    Migrated from py2neo to neo4j driver

--- bbot/db/neo4j.py
+++ bbot/db/neo4j.py
@@ -1,4 +1,4 @@
-import py2neo
+from neo4j import GraphDatabase
 import logging
 from datetime import datetime
 
@@ -14,46 +14,87 @@ class Neo4j:
     """
 
     def __init__(self, uri="bolt://localhost:7687", username="neo4j", password="bbotislife"):
-        self.graph = py2neo.Graph(uri=uri, auth=(username, password))
+        self.driver = GraphDatabase.driver(uri=uri, auth=(username, password))
 
     def insert_event(self, event):
         self.insert_events([event])
 
     def insert_events(self, events):
-        event_nodes = dict()
-        event_list = []
+        # event_nodes = dict()
+        # event_list = []
 
         for event in events:
             event_json = event.json(mode="graph")
+
             source_id = event_json.get("source", "")
             if not source_id:
                 log.warning(f"Skipping event without source: {event}")
                 continue
-            event_node = self.make_node(event_json)
-            event_nodes[event.id] = event_node
-            event_list.append(event_node)
+            event_type = event_json.pop("type")
+            relation_type = event_json.pop("module", "TARGET")
+            dest_id = event_json.get("id", "")
+            # timestamp = datetime.fromtimestamp(event_json.pop("timestamp"))
+            datetime.fromtimestamp(event_json.pop("timestamp"))
 
-        if event_nodes:
-            subgraph = list(event_nodes.values())[0]
-            for dest_event in event_list:
-                module = dest_event.pop("module", "TARGET")
-                source_id = dest_event["source"]
-                source_type = source_id.split(":")[0]
-                try:
-                    source_event = event_nodes[source_id]
-                except KeyError:
-                    source_event = self.make_node({"type": source_type, "id": source_id})
-                timestamp = datetime.fromtimestamp(dest_event.pop("timestamp"))
-                relation = py2neo.Relationship(source_event, module, dest_event, timestamp=timestamp)
-                subgraph = subgraph | relation
+            self.make_node(event_type, event_json)
+            if source_id is not dest_id:
+                self.make_relationship(event_type, source_id, relation_type, dest_id)
+                # "{event} {event_json.get(str(event), "")}"
+            # log.warning(exec_statement)
 
-            self.graph.merge(subgraph)
+    def make_node(self, event_type, event_json):
+        exec_statement = f"MERGE (:{event_type} " + "{"
+        for event in event_json:
+            if "scope_distance" in str(event) or "tags" in str(event):
+                myString = f"{event}: {event_json.get(str(event))},"
+            else:
+                myString = f'{event}: "{event_json.get(str(event))}",'
+            # log.warning(myString)
+            exec_statement += myString
+        exec_statement = exec_statement[:-1]
+        exec_statement += "})"
 
-    @staticmethod
-    def make_node(event):
-        event = dict(event)
-        event_type = event.pop("type")
-        event_node = py2neo.Node(event_type, **event)
-        event_node.__primarylabel__ = event_type
-        event_node.__primarykey__ = "id"
-        return event_node
+        # log.warning(exec_statement)
+
+        session = self.driver.session()
+        session.run(exec_statement)
+
+    def make_relationship(self, event_type, source_id, relation_type, dest_id):
+        source_type = source_id.split(":")[0]
+        # _source_id = source_id.split(":")[1]
+        dest_type = dest_id.split(":")[0]
+        # _dest_id = dest_id.split(":")[1]
+        # log.warning(f"source_type: {source_type} - source_id: {_source_id} - relation_type: {relation_type} - dest_id: {dest_id}")
+
+        exec_statement = (
+            f'MATCH (source:{source_type} {{id: "{source_id}"}}) '
+            f'MATCH (target:{dest_type} {{id: "{dest_id}"}}) '
+            f"MERGE (source)-[r:{relation_type}]->(target) "
+            f"RETURN COUNT(r) AS total"
+        )
+
+        # query = ("MATCH (source: $source_type, id: $source_id) "
+        #          "MATCH (target: $dest_type, id: $dest_id) "
+        #          "MERGE (source)-[r: $relation_type]->(target) "
+        #          "RETURN COUNT(r) AS total")
+
+        # log.warning(exec_statement)
+
+        session = self.driver.session()
+        # result = session.run(query,
+        #                      source_type=source_type,
+        #                      source_id=source_id,
+        #                      dest_type=dest_type,
+        #                      dest_id=dest_id,
+        #                      relation_type=relation_type)
+
+        result = session.run(exec_statement)
+        # exec_result = session.execute_read(exec_statement, "RETURN COUNT(r) AS total")
+
+        # log.warning(f"Result: {result}")
+
+        record = result.single()
+        # log.warning(f"Total Relationships: {record['total']}")
+
+        if record["total"] == 0:
+            self.make_relationship(event_type, source_id, relation_type, dest_id)
