1ab42770765632e660505ec5c328cfdbd1cca76d
TheTechromancer <thetechromancer@protonmail.com>
2024-03-19 15:00:08 -0400

    social dependency

--- bbot/core/modules.py
+++ bbot/core/modules.py
@@ -37,7 +37,7 @@ class ModuleLoader:
     module_dir_regex = re.compile(r"^[a-z][a-z0-9_]*$")
 
     # if a module consumes these event types, automatically assume these dependencies
-    default_module_deps = {"HTTP_RESPONSE": "httpx", "URL": "httpx"}
+    default_module_deps = {"HTTP_RESPONSE": "httpx", "URL": "httpx", "SOCIAL": "social"}
 
     def __init__(self):
         self.core = CORE
