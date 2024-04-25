12619423117b46a1ec70337c3c2c327e00830f78
TheTechromancer <thetechromancer@protonmail.com>
2024-04-25 09:35:09 -0400

    lowercase waf attribute

--- bbot/test/test_step_2/module_tests/test_module_wafw00f.py
+++ bbot/test/test_step_2/module_tests/test_module_wafw00f.py
@@ -11,7 +11,7 @@ class TestWafw00f(ModuleTestBase):
         module_test.set_expect_requests(expect_args=expect_args, respond_args=respond_args)
 
     def check(self, module_test, events):
-        assert any(e.type == "WAF" and "LiteSpeed" in e.data["WAF"] for e in events)
+        assert any(e.type == "WAF" and "LiteSpeed" in e.data["waf"] for e in events)
 
 
 class TestWafw00f_noredirect(ModuleTestBase):
