906b4440ef8bea4efd9700fe7c3e7bfa5040406c
TheTechromancer <thetechromancer@protonmail.com>
2024-04-29 16:42:27 -0400

    fix more tests

--- bbot/test/test_step_2/module_tests/test_module_csv.py
+++ bbot/test/test_step_2/module_tests/test_module_csv.py
@@ -8,4 +8,4 @@ class TestCSV(ModuleTestBase):
     def check(self, module_test, events):
         csv_file = module_test.scan.home / "output.csv"
         with open(csv_file) as f:
-            assert "DNS_NAME,blacklanternsecurity.com,,TARGET" in f.read()
+            assert "blacklanternsecurity.com,127.0.0.5,TARGET" in f.read()
