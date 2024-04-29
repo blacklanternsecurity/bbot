e66d0d054ba26f17f4b19471440564c3003d64d9
TheTechromancer <thetechromancer@protonmail.com>
2024-04-29 16:32:10 -0400

    fix more tests

--- bbot/test/test_step_2/module_tests/test_module_csv.py
+++ bbot/test/test_step_2/module_tests/test_module_csv.py
@@ -3,7 +3,7 @@ from .base import ModuleTestBase
 
 class TestCSV(ModuleTestBase):
     async def setup_after_prep(self, module_test):
-        await module_test.mock_dns({})
+        await module_test.mock_dns({"blacklanternsecurity.com": {"A": ["127.0.0.5"]}})
 
     def check(self, module_test, events):
         csv_file = module_test.scan.home / "output.csv"
