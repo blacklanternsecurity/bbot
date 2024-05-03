722e961e4cc2af279457c14bd30b1e64ea83a2dc
TheTechromancer <thetechromancer@protonmail.com>
2024-05-03 17:06:57 -0400

    remove unneeded filter

--- bbot/modules/base.py
+++ bbot/modules/base.py
@@ -730,13 +730,6 @@ class BaseModule:
         if self._is_graph_important(event):
             return True, "event is critical to the graph"
 
-        # don't send out-of-scope targets to active modules (excluding portscanners, because they can handle it)
-        # this only takes effect if your target and whitelist are different
-        # TODO: the logic here seems incomplete, it could probably use some work.
-        if "active" in self.flags and "portscan" not in self.flags:
-            if "target" in event.tags and event not in self.scan.whitelist:
-                return False, "it is not in whitelist and module has active flag"
-
         # check scope distance
         filter_result, reason = self._scope_distance_check(event)
         if not filter_result:
