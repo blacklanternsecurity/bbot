f67b259169ba197804b32741fc70066d297beabf
TheTechromancer <thetechromancer@protonmail.com>
2024-05-12 12:54:18 -0400

    fix minor race condition

--- bbot/modules/internal/cloud.py
+++ bbot/modules/internal/cloud.py
@@ -12,7 +12,6 @@ class cloud(InterceptModule):
         return True
 
     def get_dummy_modules(self):
-        self.dummy_modules = {}
         for provider_name, provider in self.helpers.cloud.providers.items():
             self.dummy_modules[provider_name] = self.scan._make_dummy_module(f"cloud_{provider_name}", _type="scan")
 
@@ -24,6 +23,7 @@ class cloud(InterceptModule):
     async def handle_event(self, event, kwargs):
         # don't hold up the event loop loading cloud IPs etc.
         if self.dummy_modules is None:
+            self.dummy_modules = {}
             await self.helpers.run_in_executor(self.get_dummy_modules)
         # cloud tagging by hosts
         hosts_to_check = set(str(s) for s in event.resolved_hosts)
