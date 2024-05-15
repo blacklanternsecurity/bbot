63ddd6e90087caf273ec1143808070ebe7c4947a
TheTechromancer <thetechromancer@protonmail.com>
2024-05-15 17:02:42 -0400

    fix gowitness

--- bbot/modules/gowitness.py
+++ bbot/modules/gowitness.py
@@ -1,3 +1,4 @@
+import asyncio
 import sqlite3
 import multiprocessing
 from pathlib import Path
@@ -164,7 +165,7 @@ class gowitness(BaseModule):
         try:
             async for line in self.run_process_live(self.command, input=stdin, idle_timeout=self.idle_timeout):
                 self.debug(line)
-        except TimeoutError:
+        except asyncio.exceptions.TimeoutError:
             urls_str = ",".join(event_dict)
             self.warning(f"Gowitness timed out while visiting the following URLs: {urls_str}", trace=False)
             return
