f488d8a218849631668ee474f5bed01f7f55652e
Dom Whewell <dom.whewell@sage.com>
2024-06-07 20:11:43 +0100

    split `build-essential` into `make` and `gcc` also combine ruby debs

--- bbot/modules/wpscan.py
+++ bbot/modules/wpscan.py
@@ -30,32 +30,17 @@ class wpscan(BaseModule):
         "disable_tls_checks": "Disables the SSL/TLS certificate verification (Default True)",
         "force": "Do not check if the target is running WordPress or returns a 403",
     }
-    deps_apt = ["curl", "build-essential"]
+    deps_apt = ["curl", "make", "gcc"]
     deps_ansible = [
         {
-            "name": "Install Rubygems (Non-Debian)",
-            "package": {"name": "rubygems", "state": "present"},
+            "name": "Install Ruby Deps (Non-Debian)",
+            "package": {"name": ["rubygems", "ruby-devel"], "state": "present"},
             "become": True,
             "when": "ansible_facts['os_family'] != 'Debian'",
         },
         {
-            "name": "Install Ruby Development Environment (Non-Debian)",
-            "package": {"name": "ruby-devel", "state": "present"},
-            "become": True,
-            "when": "ansible_facts['os_family'] != 'Debian'",
-        },
-        {
-            "name": "Install Rubygems (Debian)",
-            "package": {
-                "name": "ruby-rubygems",
-                "state": "present",
-            },
-            "become": True,
-            "when": "ansible_facts['os_family'] == 'Debian'",
-        },
-        {
-            "name": "Install Ruby Development Environment (Debian)",
-            "package": {"name": "ruby-dev", "state": "present"},
+            "name": "Install Ruby Deps (Debian)",
+            "package": {"name": ["ruby-rubygems", "ruby-dev"], "state": "present"},
             "become": True,
             "when": "ansible_facts['os_family'] == 'Debian'",
         },
