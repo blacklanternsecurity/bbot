b9c6bd00184ae5f2030e68bdc8a540ed5b435bb5
liquidsec <paul.mueller08@gmail.com>
2024-06-18 09:41:37 -0400

    more pytest output tweaks

--- bbot/test/conftest.py
+++ bbot/test/conftest.py
@@ -180,15 +180,13 @@ def proxy_server():
     server.shutdown()
     server_thread.join()
 
-# ANSI color codes
-RED = "\033[1;31m"
-GREEN = "\033[1;32m"
-YELLOW = "\033[1;33m"
-BLUE = "\033[1;34m"  # Blue for the test names
-CYAN = "\033[1;36m"  # Cyan for file and line number
-RESET = "\033[0m"
-
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
+    RED = "\033[1;31m"
+    GREEN = "\033[1;32m"
+    YELLOW = "\033[1;33m"
+    BLUE = "\033[1;34m"
+    CYAN = "\033[1;36m"
+    RESET = "\033[0m"
     stats = terminalreporter.stats
     total_tests = len(terminalreporter._session.items)
     passed = len(stats.get('passed', []))
