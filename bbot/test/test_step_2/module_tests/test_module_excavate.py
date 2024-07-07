bc4483fe8fb0c88b947f7abc6d15d68ad68543f7
liquidsec <paul.mueller08@gmail.com>
2024-07-07 15:20:52 -0400

    fixing multuple same header extraction

--- bbot/test/test_step_2/module_tests/test_module_excavate.py
+++ bbot/test/test_step_2/module_tests/test_module_excavate.py
@@ -729,3 +729,38 @@ class TestExcavateSpiderDedupe(ModuleTestBase):
         assert found_url_unverified_spider_max, "Excavate failed to find /spider link"
         assert found_url_unverified_dummy, "Dummy module did not correctly re-emit"
         assert found_url_event, "URL was not emitted from non-spider-max URL_UNVERIFIED"
+
+
+class TestExcavateHeaders(ModuleTestBase):
+
+    targets = ["http://127.0.0.1:8888/"]
+    modules_overrides = ["excavate", "httpx"]
+    config_overrides = {"web": {"spider_distance": 1, "spider_depth": 1}}
+
+    async def setup_before_prep(self, module_test):
+
+        module_test.httpserver.expect_request("/").respond_with_data(
+            "<html><p>test</p></html>",
+            status=200,
+            headers={
+                "Set-Cookie": [
+                    "COOKIE1=aaaa; Secure; HttpOnly",
+                    "COOKIE2=bbbb; Secure; HttpOnly; SameSite=None",
+                ]
+            },
+        )
+
+    def check(self, module_test, events):
+
+        found_first_cookie = False
+        found_second_cookie = False
+
+        for e in events:
+            if e.type == "WEB_PARAMETER":
+                if e.data["name"] == "COOKIE1":
+                    found_first_cookie = True
+                if e.data["name"] == "COOKIE2":
+                    found_second_cookie = True
+
+        assert found_first_cookie == True
+        assert found_second_cookie == True
