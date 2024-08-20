c1415cc72e615c17af89ee2d58e5a4d382548c35
TheTechromancer <20261699+TheTechromancer@users.noreply.github.com>
2024-08-20 16:47:08 -0400

    Update test_module_trufflehog.py

--- bbot/test/test_step_2/module_tests/test_module_trufflehog.py
+++ bbot/test/test_step_2/module_tests/test_module_trufflehog.py
@@ -852,7 +852,7 @@ class TestTrufflehog(ModuleTestBase):
             and (e.data["host"] == "hub.docker.com" or e.data["host"] == "github.com")
             and "Verified Secret Found." in e.data["description"]
             and "Raw result: [https://admin:admin@the-internet.herokuapp.com]" in e.data["description"]
-            and "RawV2 result: [https://admin:admin@the-internet.herokuapp.com/basic_auth]"
+            and "RawV2 result: [https://admin:admin@the-internet.herokuapp.com/basic_auth]" in e.data["description"]
         ]
         assert 3 == len(vuln_events), "Failed to find secret in events"
         github_repo_event = [e for e in vuln_events if "test_keys" in e.data["description"]][0].parent
