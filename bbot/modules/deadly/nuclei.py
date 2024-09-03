fa130612ec694768a22f27b8c924fe80b07742f0
blsaccess <info@blacklanternsecurity.com>
2024-09-03 21:05:11 +0000

    Update nuclei

--- bbot/modules/deadly/nuclei.py
+++ bbot/modules/deadly/nuclei.py
@@ -15,7 +15,7 @@ class nuclei(BaseModule):
     }
 
     options = {
-        "version": "3.3.0",
+        "version": "3.3.1",
         "tags": "",
         "templates": "",
         "severity": "",
