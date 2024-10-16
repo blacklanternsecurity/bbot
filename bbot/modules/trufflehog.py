574e88f9ccbdfe5e4b03f28f042be002695a382d
GitHub <noreply@github.com>
2024-10-16 00:25:19 +0000

    Update trufflehog

--- bbot/modules/trufflehog.py
+++ bbot/modules/trufflehog.py
@@ -13,7 +13,7 @@ class trufflehog(BaseModule):
     }
 
     options = {
-        "version": "3.82.8",
+        "version": "3.82.9",
         "config": "",
         "only_verified": True,
         "concurrency": 8,
