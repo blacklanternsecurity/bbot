04206e21d06bed496d1775b725a9be3773e1a02d
liquidsec <paul.mueller08@gmail.com>
2025-10-17 15:41:04 -0400

    fix test again

--- bbot/test/test_step_2/module_tests/test_module_generic_ssrf.py
+++ bbot/test/test_step_2/module_tests/test_module_generic_ssrf.py
@@ -32,13 +32,11 @@ class TestGeneric_SSRF(ModuleTestBase):
 
         return Response("alive", status=200)
 
-    async def setup_before_prep(self, module_test):
+    async def setup_after_prep(self, module_test):
         self.interactsh_mock_instance = module_test.mock_interactsh("generic_ssrf")
         module_test.monkeypatch.setattr(
             module_test.scan.helpers, "interactsh", lambda *args, **kwargs: self.interactsh_mock_instance
         )
-
-    async def setup_after_prep(self, module_test):
         expect_args = re.compile("/")
         module_test.set_expect_requests_handler(expect_args=expect_args, request_handler=self.request_handler)
 
